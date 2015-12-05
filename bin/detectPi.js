e1e7b37c22006b33d0dadc2713da5f909094bdc0
v1rgul <v1rgul.42@gmail.com>
2015-12-05 09:39:15 +0100

    log tatus of Pi detection

--- bin/detectPi.js
+++ bin/detectPi.js
@@ -3,10 +3,13 @@
 var fs = require('fs');
 
 function detectPi(){
+	var log = require('log4js').getLogger('detectPi');
 	try {
 		fs.statSync("/sys/class/gpio/gpiochip0");
+		log.info("found Pi!");
 		return true;
-	} catch (e) {}	
+	} catch (e) {}
+	log.warn("not found Pi files !");
 	return false;
 }
 
